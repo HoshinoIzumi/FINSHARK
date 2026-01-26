6a72bc92ec4729779541081dad29f38fa3de66e9
HoshinoIzumi <hoshinoitsumi@gmail.com>
2026-01-26 23:15:14 +1100

    add initial Comment and Stocks model classes

---
+++ api/Models/Comment.cs
@@ -0,0 +1,12 @@
+namespace api.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; } = String.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    //Foreign Key
+    public int? StockId { get; set; }
+    public Stock? Stock { get; set; }
+}
